7da94165c30073d14ead0e459528666698d672d0
SuperGarryGamer <melentyev.jay@gmail.com>
2024-11-12 17:22:41 +0100

    type casting

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ can.initialize()
 
 try:
     while True:
-        print("RX: " + can.poll_receive())
+        print("RX: " + str(can.poll_receive()))
         can_id = int(random.random() * 0x7FF)
-        print("TX: " + [can_id] + [ord(c) for c in "owo"])
+        print("TX: " + str(([can_id], [ord(c) for c in "owo"])))
         can.transmit(can_id, bytes("owo", "ASCII"))
         time.sleep(1)
 

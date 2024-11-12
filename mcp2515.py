3ca728b7e03e4896107736fda3fc51eed8808e58
SuperGarryGamer <melentyev.jay@gmail.com>
2024-11-12 17:06:36 +0100

    more padding

--- mcp2515.py
+++ mcp2515.py
@@ -126,7 +126,7 @@ def transmit(can_id: int, message: bytes):
     spi.xfer2(0b10000001.to_bytes())
 
 def poll_receive():
-    filler = 0x00.to_bytes() * 8 # 64 bits of padding to receive 8 bytes of data from CAN
+    filler = 0x00.to_bytes() * 13 * 8 # enough padding to receive full message
     instruction = 0b10010000.to_bytes() + filler
 
     result = spi.xfer2(instruction)

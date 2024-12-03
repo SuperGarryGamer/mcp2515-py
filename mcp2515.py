9d1642f8b6fe22dccf5acdc53960842ab2818855
SuperGarryGamer <melentyev.jay@gmail.com>
2024-12-03 04:24:52 +0100

    initial commit

---
+++ mcp2515.py
@@ -0,0 +1,42 @@
+import spidev
+import queue
+
+SPEED = 10_000_000 # 10 MHz SPI
+
+spi = None
+
+class Can_Frame:
+    def __init__(self):
+        self.sid = 0
+        self.data = []
+
+def initialize():
+    spi = spidev.SpiDev()
+    spi.open(0, 0)
+    spi.max_speed_hz = SPEED
+    spi.mode = 0b00
+
+    reset()
+
+    # Switch to normal mode
+    spi.writebytes(bytes([0x02, 0x0f, 0x00]))
+
+
+def transmit_frame(frame: Can_Frame):
+    if len(frame.data) > 8:
+        raise ValueError
+    if frame.sid > 0x07ff:
+        raise ValueError
+    
+    to_transmit = [0x40, frame.sid >> 3, (frame.sid & 0x07) << 5, 0x00, 0x00, len(frame.data)]
+
+    for i in range(len(frame.data)):
+        to_transmit.append(frame.data[i])
+
+    spi.writebytes(bytes(to_transmit))
+    spi.writebytes(bytes([0x81]))
+
+def reset():
+    spi.writebytes(bytes([0xc0]))
+
+# Testing:
